9019108b8fea57964cc024649e28227f39da5679
Soesapto Joeni Hantoro <shidec00@yahoo.com>
2018-10-18 10:57:49 +0700

    comment on htaccess

--- folder_select.php
+++ folder_select.php
@@ -7,13 +7,10 @@ $file = $link_array[count($link_array) - 2];
 
 #select file from the right folder
 $name = "{$size}/{$file}.png";
-#open the file in a binary mode
-$fp = fopen($name, 'rb');
 
 #send the right headers
 header("Content-Type: image/png");
 header("Content-Length: " . filesize($name));
 
-#dump the picture and close the file
-fpassthru($fp);
-fclose($fp);
+#Reads a file and writes it to the output buffer
+readfile($name);
